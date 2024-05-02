98e865ea3c85a5ff369cb59d4226421625dc6bb8
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-05-02 16:47:10 +0530

    Multiple Routes Created (Contacts CRUD Route Setup)

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,20 @@ router.route("/").get((req, res) => {
     res.status(200).json({message: "Get All Contacts."});
 });
 
+router.route("/").post((req, res) => {
+    res.status(200).json({message: "Create Contact."});
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json({message: `Get Contact For ${req.params.id}.`});
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).json({message: `Update Contact For ${req.params.id}.`});
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).json({message: `Delete Contact For ${req.params.id}.`});
+});
+
 module.exports = router;
