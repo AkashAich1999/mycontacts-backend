7b35cea5d0c8a80215a30bcec201915e570ed106
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-05-03 10:45:23 +0530

    If some or all input fields are Empty, Throw Error.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,6 +4,11 @@ const getContacts = (req, res) => {
 
 const createContact = (req, res) => {
     console.log("The Request Body is: ", req.body);
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are Mandatory !");
+    }
     res.status(201).json({message: "Create Contact."});
 }
 
