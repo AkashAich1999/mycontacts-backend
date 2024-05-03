0a9255c9d7a932b6058bb6444ff0cce5a0ab8bfa
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-05-03 10:36:28 +0530

    Built-In Middleware for POST Request Body : app.use(express.json());

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,6 +3,7 @@ const getContacts = (req, res) => {
 }
 
 const createContact = (req, res) => {
+    console.log("The Request Body is: ", req.body);
     res.status(201).json({message: "Create Contact."});
 }
 
