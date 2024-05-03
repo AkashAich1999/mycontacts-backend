36503dee241e9017b70ab7908048be1cd5fe02cb
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-05-03 11:23:30 +0530

    Error Handling Middleware Done

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,9 @@
+const { constant } = require("../constant.js");
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   switch (statusCode) {
-    case 400: 
+    case constant.VALIDATION_ERROR:
       res.json({
         title: "Validation Failed",
         message: err.message,
@@ -9,7 +11,7 @@ const errorHandler = (err, req, res, next) => {
       });
       break;
 
-    case 404:
+    case constant.NOT_FOUND:
       res.json({
         title: "Not Found",
         message: err.message,
@@ -17,7 +19,32 @@ const errorHandler = (err, req, res, next) => {
       });
       break;
 
+    case constant.UNAUTHORIZED:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constant.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constant.SERVER_ERROR:
+      res.json({
+        title: "Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
     default:
+      console.log("No Error. All Good !");  
       break;
   }
 };
