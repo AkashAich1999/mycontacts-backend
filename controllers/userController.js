6a81e9f3665413739536126119dedd199350fdc0
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-05-04 20:32:36 +0530

    User Registration & Password Hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,39 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req,res) => {
     const { username, email, password } = req.body;
     if(!username || !email || !password){
         res.status(400);
-        
+        throw new Error("All Fields are Mandaatory.")
     }
+
+    const userAvailable = await User.findOne({ email });
+    console.log(userAvailable);
+    if(userAvailable){
+        res.status(400);
+        throw new Error("All Already Registered.")        
+    }
+
+    // Hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    console.log(`User created ${user}`);
+    if (user) {
+       res.status(201).json({ _id:user.id, email:user.email });
+    } else {
+       res.status(400);
+       throw new Error("User Data is Not Valid");      
+    }
+
     res.json({ message: "Register The User" });
 });
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mmongoose");
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     username:{
